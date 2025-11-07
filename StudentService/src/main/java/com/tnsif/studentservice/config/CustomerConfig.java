25813b36b7d858bd58791a22fb62d002758b7b00
hamjath <syedanwar@gmail.com>
2025-11-07 22:26:12 +0530

    Config File Updated

--- StudentService/src/main/java/com/tnsif/studentservice/config/CustomerConfig.java
+++ StudentService/src/main/java/com/tnsif/studentservice/config/CustomerConfig.java
@@ -1,19 +1,26 @@
 package com.tnsif.studentservice.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-@Configuration
-@EnableWebMvc
-public class CustomerConfig implements WebMvcConfigurer {
-	
-	@Override
-	public void addCorsMappings(CorsRegistry registry) {
-		registry.addMapping("/**")
-			.allowedOrigins("http://localhost:3000/")
-			.allowedMethods("GET", "POST", "PUT", "DELETE");
-	}
 
+@Configuration
+@CrossOrigin(origins = "http://localhost:3000")  // React connection
+public class CustomerConfig {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("http://localhost:3000")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE")
+                        .allowedHeaders("*");
+            }
+        };
+    }
 }
